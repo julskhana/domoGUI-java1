2b57c33f6ccc23756d5977f722a150c6d4cb5ec3
Julian Bajana <juls_tier5@hotmail.com>
2016-08-08 13:03:49 -0500

    actualizacion funciones

--- src/funciones/Validacion.java
+++ src/funciones/Validacion.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package funciones;
 
 import java.util.ArrayList;
 import dispositivos.*;
 
 /**
- *
  * @author julian
  */
 public class Validacion {
